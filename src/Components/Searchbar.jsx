4209c2b9a80cffdc82ea99528d14664aeb36eea6
johndevnoza <ioandevnoza@gmail.com>
2023-12-01 17:37:58 +0400

    bit fix

--- src/Components/Searchbar.jsx
+++ src/Components/Searchbar.jsx
@@ -1,28 +1,26 @@
 import React, { useState, useEffect } from "react";
-import { useSearchParams, useLocation } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import { FontAwesomeIcon } from "../assets/Icons/icons";
-import "./components.css"
+import "./components.css";
 
 function SearchBar() {
   const [searchQuery, setSearchQuery] = useState("");
   const [searchParams, setSearchParams] = useSearchParams();
-  const location = useLocation();
 
   useEffect(() => {
-    // Use location to get the search query from the URL on component mount
-    const params = new URLSearchParams(location.search);
-    const query = params.get("q");
+    const query = searchParams.get("q");
     if (query) {
       setSearchQuery(query);
     }
-  }, [location.search]);
+  }, [searchParams]);
 
   const handleSearch = () => {
-    if (searchQuery.trim() !== "") {
-      setSearchParams({ q: searchQuery });
-    } else {
-      // If the search query is empty, clear the "q" parameter from the URL
-      setSearchParams({});
+    setSearchParams({ q: searchQuery }, { replace: true });
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleSearch();
     }
   };
 
@@ -32,6 +30,7 @@ function SearchBar() {
         type="text"
         value={searchQuery}
         onChange={(e) => setSearchQuery(e.target.value)}
+        onKeyDown={handleKeyPress}
       />
       <FontAwesomeIcon
         icon="search"
