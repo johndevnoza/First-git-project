c16bf3da8d5ef9d58b3eb6c5cca9b6da0b2dd7fa
johndevnoza <ioandevnoza@gmail.com>
2023-12-09 18:02:25 +0400

    Cart system

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,27 @@ import Shopping from "./Pages/Shopping";
 import ProductsPage from "./Pages/products/ProductsPage";
 import SingleProduct from "./Pages/products/SingleProductpage";
 import SingleCategoryPage from "./Pages/categories/SingleCategoryPage";
+import { ShoppingCartProvider } from "./Services/ShoppingCartContext";
 import NotFound from "./Pages/NotFound";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="/" element={<ProductsPage />} />
-        <Route path="/products/:itemId" element={<SingleProduct />} />
-        <Route path="/products/category/:id" element={<SingleCategoryPage />} />
-        <Route path="/shopping" element={<Shopping />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    </Routes>
+    <ShoppingCartProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<ProductsPage />} />
+          <Route path="/products/:itemId" element={<SingleProduct />} />
+          <Route
+            path="/products/category/:id"
+            element={<SingleCategoryPage />}
+          />
+          <Route path="/shopping" element={<Shopping />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </ShoppingCartProvider>
   );
 }
 
 export default App;
-
-
--- src/Components/Common/Card.jsx
+++ src/Components/Common/Card.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 import "./common.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import useShoppingCart from "../../Services/ShoppingCartContext";
 
-export default function Card({ title, price, description, category, image, className="card" }) {
+export default function Card({
+  title,
+  price,
+  description,
+  category,
+  image,
+  id,
+  className = "card",
+}) {
+  const {
+    getItemQuantity,
+    increaseCartQuantity,
+    decreaseCartQuantity,
+    removeFromCart,
+  } = useShoppingCart();
+
+  const quantity = getItemQuantity(id);
   return (
     <div className={className}>
       <img src={image} alt="title" />
@@ -9,8 +27,55 @@ export default function Card({ title, price, description, category, image, class
       <h4>{title}</h4>
       <p>{description}</p>
       <div className="price-Cart">
-        <span>Price: {price}$</span>
-        <div className="addButton">Add</div>
+        <span>{price}$</span>
+        <FontAwesomeIcon className="favoritesIcon" icon="heart" />
+        {quantity === 0 ? (
+          <FontAwesomeIcon
+            className="addButton"
+            icon="cart-shopping"
+            onClick={(event) => {
+              event.stopPropagation();
+              event.preventDefault(); // Prevent the link from being executed
+              increaseCartQuantity(id);
+            }}
+          />
+        ) : (
+          <>
+            <div className="changeNumber">
+              <div
+                className="numberControl"
+                onClick={(event) => {
+                  event.stopPropagation();
+                  event.preventDefault(); // Prevent the link from being executed
+                  decreaseCartQuantity(id);
+                }}
+              >
+                -
+              </div>
+              <div>{quantity}</div>
+              <div
+                className="numberControl"
+                onClick={(event) => {
+                  event.stopPropagation();
+                  event.preventDefault(); // Prevent the link from being executed
+                  increaseCartQuantity(id);
+                }}
+              >
+                +
+              </div>
+            </div>
+              <div
+                className="remove"
+                onClick={(event) => {
+                  event.stopPropagation();
+                  event.preventDefault(); // Prevent the link from being executed
+                  removeFromCart(id);
+                }}
+              >
+                X
+              </div>
+          </>
+        )}
       </div>
     </div>
   );
